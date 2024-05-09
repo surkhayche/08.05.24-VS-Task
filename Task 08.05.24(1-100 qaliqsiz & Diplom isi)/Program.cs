b4fd8faad5ccb942aa82ed4fcf263e5986fd75ed
Surkhay <surkhayshikhiyev@gmail.com>
2024-05-10 03:13:54 +0400

    Task Homework

---
+++ Task 08.05.24(1-100 qaliqsiz & Diplom isi)/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task_08._05._24_1_100_qaliqsiz___Diplom_isi_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            #region Task 2
+
+            //int examResult = 66;
+
+            //if (examResult <= 65)
+            //{
+            //    Console.WriteLine($"Kesildin");
+            //}
+            //else if (examResult > 65 && examResult < 85)
+            //{
+            //    Console.WriteLine($"Adi diplom");
+            //}
+            //else if(examResult >= 85 &&  examResult <= 95)
+            //{
+            //    Console.WriteLine($"Seref diplomu");
+            //}
+            //else if (examResult > 95)
+            //{
+            //    Console.WriteLine($"Yuksek seref diplomu");
+            //}
+
+            #endregion
+
+        }
+    }
+}
